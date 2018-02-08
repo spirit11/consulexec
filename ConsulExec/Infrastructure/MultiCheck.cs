726ae78fb97638ed36a878463e5d0dda8b62b9fc
spirit11 <mps_ymg@rambler.ru>
2018-02-08 13:44:45 +0300

    auto selector scanning

--- ConsulExec/Infrastructure/MultiCheck.cs
+++ ConsulExec/Infrastructure/MultiCheck.cs
@@ -1,52 +1,105 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using MahApps.Metro.Controls;
 
 namespace ConsulExec.Infrastructure
 {
     public class MultiCheck : DependencyObject
     {
-        public static readonly DependencyProperty GroupProperty = DependencyProperty.RegisterAttached(
-            "Group", typeof(string), typeof(MultiCheck), new PropertyMetadata(default(string)) { PropertyChangedCallback = SetGroupChangedCallback });
+        #region Selector Property
 
-        public static void SetGroup(DependencyObject element, string value)
+        public static readonly DependencyProperty SelectorProperty =
+            DependencyProperty.RegisterAttached(
+            "Selector", typeof(Selector), typeof(MultiCheck), new PropertyMetadata(default(Selector)) { PropertyChangedCallback = SelectorChangedCallback });
+
+        /// <summary>
+        /// When set to Selector then a group of selected items in it is checked or unchecked simultaneously.
+        /// </summary>
+        public static void SetSelector(DependencyObject Element, Selector Value)
         {
-            element.SetValue(GroupProperty, value);
+            Element.SetValue(SelectorProperty, Value);
         }
 
-        public static string GetGroup(DependencyObject element)
+        public static Selector GetSelector(DependencyObject Element)
         {
-            return (string)element.GetValue(GroupProperty);
+            return (Selector)Element.GetValue(SelectorProperty);
         }
 
-        private static readonly Dictionary<string, List<ToggleButton>> groups = new Dictionary<string, List<ToggleButton>>();
+        #endregion
 
-        private static void SetGroupChangedCallback(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        #region Apply property
+
+        public static readonly DependencyProperty ApplyProperty =
+            DependencyProperty.RegisterAttached(
+            "Apply", typeof(bool), typeof(MultiCheck), new PropertyMetadata(default(bool)) { PropertyChangedCallback = ApplyChangedCallback });
+
+
+        /// <summary>
+        /// When set to True then a group of selected items in parent selector is checked or unchecked simultaneously.
+        /// </summary>
+        public static void SetApply(DependencyObject Element, bool Value)
+        {
+            Element.SetValue(ApplyProperty, Value);
+        }
+
+        public static bool GetApply(DependencyObject Element)
+        {
+            return (bool)Element.GetValue(ApplyProperty);
+        }
+
+        #endregion
+
+        private static readonly Dictionary<Selector, HashSet<ToggleButton>> Groups = new Dictionary<Selector, HashSet<ToggleButton>>();
+        private static bool PreventChecking;
+
+        private static void ApplyChangedCallback(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        {
+            var toggleButton = D as ToggleButton;
+            var selector = D.TryFindParent<Selector>();
+
+            if (E.NewValue.Equals(true) && toggleButton != null && selector != null)
+                SetSelector(toggleButton, selector);
+        }
+
+        private static void SelectorChangedCallback(DependencyObject D, DependencyPropertyChangedEventArgs E)
         {
             var chk0 = D as ToggleButton;
-            if (chk0 == null)
+            if (chk0 == null || E.NewValue == null)
                 return;
-            var group = groups.GetOrAdd((string)E.NewValue, _ => new List<ToggleButton>());
-            group.Add(chk0);
-            chk0.Checked += HandleCheck;
-            chk0.Unchecked += HandleCheck;
-            chk0.Unloaded += delegate { group.Remove(chk0); };
+            AddToGroup(chk0, (Selector)E.NewValue);
         }
 
-        private static bool PreventChecking;
+        private static void AddToGroup(ToggleButton Button, Selector Selector)
+        {
+            var group = Groups.GetOrAdd(Selector, _ => new HashSet<ToggleButton>());
+            group.Add(Button);
+
+            Button.Checked += HandleCheck;
+            Button.Unchecked += HandleCheck;
+            RoutedEventHandler detach = null;
+            detach = delegate
+            {
+                Button.Unloaded -= detach;
+                group.Remove(Button);
+            };
+            Button.Unloaded += detach;
+        }
 
         private static void HandleCheck(object Sender, RoutedEventArgs E)
         {
-            if (PreventChecking)
-                return;
             var button = (ToggleButton)Sender;
+            if (PreventChecking || !IsSelectedInList(button))
+                return;
             var isChecked = button.IsChecked;
             PreventChecking = true;
             try
             {
-                foreach (var toggleButton in groups[GetGroup(button)].Where(IsSelectedInList))
+                foreach (var toggleButton in Groups[GetSelector(button)].Where(IsSelectedInList))
                     toggleButton.IsChecked = isChecked;
             }
             finally
@@ -57,10 +110,8 @@ namespace ConsulExec.Infrastructure
 
         private static bool IsSelectedInList(FrameworkElement Button)
         {
-            var lbitem = Button;
-            while (lbitem != null && !(lbitem is ListBoxItem))
-                lbitem = lbitem.TemplatedParent as FrameworkElement;
-            return (lbitem as ListBoxItem)?.IsSelected ?? false;
+            var lbitem = Button.TryFindParent<ListBoxItem>();
+            return lbitem?.IsSelected ?? false;
         }
     }
 }
