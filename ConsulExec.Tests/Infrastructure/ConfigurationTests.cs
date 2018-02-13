186fc59af11c2e9fd233d6baafcfba5abd952e71
spirit11 <mps_ymg@rambler.ru>
2018-02-13 11:15:05 +0300

    Json persistence implemented in model

---
+++ ConsulExec.Tests/Infrastructure/ConfigurationTests.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Linq;
+using ConsulExec.Domain;
+using ConsulExec.Tests.Util;
+using NUnit.Framework;
+
+namespace ConsulExec.Tests.Infrastructure
+{
+    [TestFixture]
+    public class ConfigurationTests : AssertionHelper
+    {
+        [Test]
+        public void ConfigurationIsPersistent()
+        {
+            var co = new ConnectionOptions { Name = "n", ServerAddress = "s" };
+            var co2 = new ConnectionOptions { Name = "n2", ServerAddress = "s2" };
+            var connections = new[] { co, co2 };
+            var savedTarget = new Configuration
+            {
+                Connections = connections,
+                MruCommands = new[] { "cmd1", "cmd2" },
+                Starup = new[]
+                {
+                    new SequentialStartupOptions(new[] { "a", "b" }) { Connection = connections.First() },
+                    new SequentialStartupOptions(new[] { "c", "d" }) { Connection = connections.First() },
+                    new SequentialStartupOptions(new[] { "e" }) { Connection = connections.Last() },
+                }
+            };
+
+            var loadedTarget = SaveAndLoadTarget(savedTarget);
+
+            Expect(loadedTarget.MruCommands, EqualTo(savedTarget.MruCommands),
+                "Recent commands are not saved or loaded");
+            Expect(loadedTarget.Connections, PropConstraint.Create(savedTarget.Connections, v => v.Name),
+                "Connection names are not saved or loaded");
+            Expect(loadedTarget.Connections, PropConstraint.Create(savedTarget.Connections, v => v.ServerAddress),
+                "Connection addreses are not saved or loaded");
+
+            Expect(loadedTarget.Starup.Select(v => v.Connection).Distinct(), SubsetOf(loadedTarget.Connections),
+                "Connections of startup properties are not from Connections collection");
+
+            Expect(loadedTarget.Starup, PropConstraint.Create(savedTarget.Starup, v => v.Connection.Name),
+                "Starup connections are not properly mapped");
+
+            Expect(loadedTarget.Starup, PropConstraint.Create(savedTarget.Starup, v => v.Name),
+                "Starup names are not saved or loaded");
+            Expect(loadedTarget.Starup, PropConstraint.Create(savedTarget.Starup, v => string.Join(", ", v.Nodes)),
+                "Starup nodes are not saved or loaded");
+        }
+
+        private static Configuration SaveAndLoadTarget(Configuration SavedTarget)
+        {
+            using (var sw = new StringWriter())
+            {
+                SavedTarget.SaveTo(sw);
+                using (var rd = new StringReader(sw.ToString()))
+                    return Configuration.ReadFrom(rd);
+            }
+        }
+    }
+}
---
+++ ConsulExec/Domain/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsulExec.Domain
+{
+    public class Configuration
+    {
+        public static Configuration ReadFrom(TextReader Reader)
+        {
+            return JsonConvert.DeserializeObject<Configuration>(Reader.ReadToEnd(), JsonSettings);
+        }
+
+        public string[] MruCommands;
+        public ConnectionOptions[] Connections;
+        public StartupOptions[] Starup;
+
+        public void SaveTo(TextWriter Writer)
+        {
+            var json = JsonConvert.SerializeObject(this, JsonSettings);
+            Writer.Write(json);
+        }
+
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Objects,
+            TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects
+        };
+    }
+}
